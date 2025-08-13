19d852d3fce0f8e6e17fc656d3fb0e8c890bbe3d
vernoxy <vernoxy3@gmail.com>
2025-08-13 16:54:26 +0530

    Add project files

--- src/components/Home/ProjectOverview.jsx
+++ src/components/Home/ProjectOverview.jsx
@@ -1,11 +1,20 @@
-import { Section } from "lucide-react";
 import React from "react";
 import PrimaryHeading from "../PrimaryCompo/PrimaryHeading";
+import Project from "../../assets/HomePageImgs/Projects.webp";
 
 const ProjectOverview = () => {
   return (
     <section className="py-10">
       <PrimaryHeading>Project Overview</PrimaryHeading>
+      <div className="container pl-0">
+        <div className=" bg-gradient-to-t from-[#4ED5E2]/50 to-transparent rounded-r-[70px] p-5 px-24 flex items-end justify-end">
+          <img
+            src={Project}
+            alt="Project visual"
+            className="object-cover"
+          />
+        </div>
+      </div>
     </section>
   );
 };
